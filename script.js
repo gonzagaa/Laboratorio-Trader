69615dd46f752cecd31d2b37f0f883c2ab1dc82b
gonzxgx <iigust4vo@gmail.com>
2025-05-30 17:58:09 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,120 @@
+
+AOS.init(
+  {
+      duration: 1200,
+  }
+);
+
+const larguraDaTela = window.innerWidth
+  
+if (larguraDaTela < 800) {
+    var swiper3 = new Swiper(".mySwiper3", {
+        grabCursor: true,
+        effect: "creative",
+        autoplay: {
+            delay: 3500,
+            disableOnInteraction: false,
+          },
+        creativeEffect: {
+          prev: {
+            shadow: true,
+            translate: ["-20%", 0, -1],
+          },
+          next: {
+            translate: ["100%", 0, 0],
+          },
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+} else {
+    var swiper3 = new Swiper(".mySwiper3", {
+        slidesPerView: 4,
+        spaceBetween: 20,
+        loop: true,
+        grabCursor: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+}
+
+if (larguraDaTela < 800) {
+  var swiper4 = new Swiper(".mySwiper4", {
+      grabCursor: true,
+      autoplay: {
+          delay: 3500,
+          disableOnInteraction: false,
+        },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      spaceBetween: 20,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+} else {
+  var swiper4 = new Swiper(".mySwiper4", {
+      slidesPerView: 3,
+      spaceBetween: 20,
+      loop: true,
+      grabCursor: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+}
+
+  const videoBoxes = document.querySelectorAll(".video");
+  const modal = document.getElementById("videoModal");
+  const videoContainer = document.getElementById("videoContainer");
+  const closeBtn = document.querySelector(".close");
+
+  videoBoxes.forEach(box => {
+    box.addEventListener("click", () => {
+      const videoSrc = box.getAttribute("data-video");
+
+      // Cria o elemento <video>
+      const video = document.createElement("video");
+      video.src = videoSrc;
+      video.controls = true;
+      video.autoplay = true;
+      videoContainer.innerHTML = "";
+      videoContainer.appendChild(video);
+
+      // Abre o modal
+      modal.style.display = "flex";
+    });
+  });
+
+  closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+    videoContainer.innerHTML = ""; // remove o vídeo para parar o carregamento
+  });
+
+  // Fecha o modal ao clicar fora
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.style.display = "none";
+      videoContainer.innerHTML = "";
+    }
+  });
